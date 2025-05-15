803564f2583dc8fda8e47d37edb66ee99c0bceff
G50 <savitagadadavar1994@gmail.com>
2025-05-15 23:34:57 +0530

    Comments program

---
+++ src/ex_01_Java_Basics/Lab004_Comments.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+        // This is comment, it is ignored by the compiler
+        System.out.println("Comments are ignored by the compiler");
+    }
+}
