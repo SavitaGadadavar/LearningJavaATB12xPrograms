a9a4dc169828a857aaa7de62695d2ee0839335a2
G50 <savitagadadavar1994@gmail.com>
2025-05-16 22:51:25 +0530

    Task1 5 tables

---
+++ src/Tasks/Task01.java
@@ -0,0 +1,17 @@
+package Tasks;
+
+public class Task01 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("5 x 1 = %d%n",num);
+        System.out.printf("5 x 2 = %d%n",num*2);
+        System.out.printf("5 x 3 = %d%n",num*3);
+        System.out.printf("5 x 4 = %d%n",num*4);
+        System.out.printf("5 x 5 = %d%n",num*5);
+        System.out.printf("5 x 6 = %d%n",num*6);
+        System.out.printf("5 x 7 = %d%n",num*7);
+        System.out.printf("5 x 8 = %d%n",num*8);
+        System.out.printf("5 x 9 = %d%n",num*9);
+        System.out.printf("5 x 10 = %d%n",num*10);
+    }
+}
