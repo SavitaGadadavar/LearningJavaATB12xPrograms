ab3cbb1baf8634d0639b0ae666499c7f11ae7891
G50 <savitagadadavar1994@gmail.com>
2025-05-16 21:08:33 +0530

    byte program

---
+++ src/ex_02_Java_Basics_Part2/Lab018_Bytes.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab018_Bytes {
+    public static void main(String[] args) {
+        byte age = 31;
+        System.out.println(age);
+
+    }
+
+    public static void main(int args) {
+        byte age = 35;
+        System.out.println(age);
+
+    }
+}
