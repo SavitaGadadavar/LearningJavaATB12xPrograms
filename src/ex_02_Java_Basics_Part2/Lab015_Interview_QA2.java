534e3cf64341dbe98aeb77990ce6e93da57d2646
G50 <savitagadadavar1994@gmail.com>
2025-05-16 20:53:23 +0530

    Interview QA2

---
+++ src/ex_02_Java_Basics_Part2/Lab015_Interview_QA2.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab015_Interview_QA2 {
+    public static void main(String[] args) {
+        int Enum = 10;
+        int a_b = 45;
+        //int savita gadadavar= 31;
+    int savita$gadadavar =31;
+        int lab015_Interview_QA= 56;
+    }
+}
