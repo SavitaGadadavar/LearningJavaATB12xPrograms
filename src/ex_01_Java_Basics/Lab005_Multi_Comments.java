8052aa3b6f20ca460c2b9506caa5d49cd7f7eddf
G50 <savitagadadavar1994@gmail.com>
2025-05-16 09:15:25 +0530

    Multiple Comments program

---
+++ src/ex_01_Java_Basics/Lab005_Multi_Comments.java
@@ -0,0 +1,24 @@
+package ex_01_Java_Basics;
+
+
+public class Lab005_Multi_Comments {
+    /**
+     * Author : Savita
+     * Course :  ATB12x
+     * Learning :  Java
+     *
+     **/
+    public static void main(String[] args) {
+        /**
+         * Author : Savita
+         * Course :  ATB12x
+         * Learning :  Java
+         *
+         **/
+        System.out.println("Multiple comments");
+    }
+
+
+
+    }
+
