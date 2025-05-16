9b260fb88634a06b9152a7c4dca64b1eeb551c1d
G50 <savitagadadavar1994@gmail.com>
2025-05-16 09:32:59 +0530

    print vs println program

---
+++ src/ex_01_Java_Basics/Lab008_Println_vs_Print.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab008_Println_vs_Print {
+    public static void main(String[] args) {
+        System.out.println("New line");
+        System.out.print("No new line");
+        System.out.print("No new line here");
+    }
+}
