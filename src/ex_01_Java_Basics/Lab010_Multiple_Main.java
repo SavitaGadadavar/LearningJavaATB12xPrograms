42a93c707530553177d14481f8cbb2e04b4f6184
G50 <savitagadadavar1994@gmail.com>
2025-05-16 09:39:33 +0530

    Multiple main program

---
+++ src/ex_01_Java_Basics/Lab010_Multiple_Main.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basics;
+
+public class Lab010_Multiple_Main {
+    public static void main(String[] args) {
+    }
+
+    public static void main(int args) {
+    }
+
+    public static void main(String args) {
+
+    }
+
+    }
+
+
