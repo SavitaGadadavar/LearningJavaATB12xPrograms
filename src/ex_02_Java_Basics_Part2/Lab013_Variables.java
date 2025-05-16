c446076b2cfb6c8802e0e1ca7e10487530e546ec
G50 <savitagadadavar1994@gmail.com>
2025-05-16 20:40:48 +0530

    variables program3

---
+++ src/ex_02_Java_Basics_Part2/Lab013_Variables.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab013_Variables {
+    public static void main(String[] args) {
+        int age = 45;
+        age = age+1;
+        age = 35;
+        System.out.println(age);
+    }
+}
