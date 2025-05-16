3c8b7fa1e01b2869d2834a6b914891b7535022be
G50 <savitagadadavar1994@gmail.com>
2025-05-16 20:47:01 +0530

    Interview QA1

---
+++ src/ex_02_Java_Basics_Part2/Lab014_Interview_QA.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab014_Interview_QA {
+    public static void main(String[] args) {
+        int _2 = 123;
+        int _1 = 1234;
+        int _0 = 12345;
+        System.out.println(_2);
+    }
+}
