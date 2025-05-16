f1054ed80d61536928c1fcc8e0812cbe9cf2828b
G50 <savitagadadavar1994@gmail.com>
2025-05-16 20:35:56 +0530

    variables program2

---
+++ src/ex_02_Java_Basics_Part2/Lab012_Variables.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab012_Variables {
+    public static void main(String[] args) {
+        int a = 20;
+        a = 70 + 1;
+        System.out.println(a);
+
+    }
+}
