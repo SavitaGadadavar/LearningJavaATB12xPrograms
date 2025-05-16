316b0632a668c450f97ded9d300ca2eda947e41b
G50 <savitagadadavar1994@gmail.com>
2025-05-16 09:20:16 +0530

    Keywords and identifiers program

---
+++ src/ex_01_Java_Basics/Lab006_Keywords_identifiers.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab006_Keywords_identifiers {
+    public static void main(String[] args) {
+        System.out.println("Hi Keyword!");
+        System.out.println("Bye");
+    }
+}
