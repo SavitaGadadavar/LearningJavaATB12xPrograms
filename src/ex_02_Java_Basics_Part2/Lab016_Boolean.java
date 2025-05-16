f1a91424d99f55cef4953f22e26f1043a476a963
G50 <savitagadadavar1994@gmail.com>
2025-05-16 20:58:48 +0530

    Boolean program

---
+++ src/ex_02_Java_Basics_Part2/Lab016_Boolean.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab016_Boolean {
+    public static void main(String[] args) {
+        boolean is_savita_married = true;
+        Boolean savita_have_iphone = false;
+    }
+}
